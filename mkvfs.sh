408639e3dbd86dd6ea6720254745df1bda596bf4
Nov11 <529079634@qq.com>
2018-02-12 18:51:16 +0800

    adopted vfs. this is a, not really a vfs. but some codes only. not clear how to specify compile flags to different targets.

--- mkvfs.sh
+++ mkvfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo 'mkvfs'
-gcc src/tools/ramGenerator.c  -I src/ -o gen
+cd $1
+gcc $1/src/tools/ramGenerator.c  -I src/ -o gen
 ./gen test.txt test.txt test2.txt  test2.txt
 ld -m elf_i386 --build-id=none -r -b binary initrd.img -o init.o
+cd -
